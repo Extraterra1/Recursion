8f56f8609911ea7e106e0152f3ab0bd12983452b
Carlos Pires <extraterra1@gmail.com>
2023-09-30 23:13:27 +0100

    add fibonacciRecursive

---
+++ fibonacciRecursive.js
@@ -0,0 +1,10 @@
+function fib(n, arr = [0, 1]) {
+  if (n <= 1) return 1;
+  if (arr.length >= n) return arr;
+  const newNumber = arr[arr.length - 1] + arr[arr.length - 2];
+  arr.push(newNumber);
+
+  return fib(n, arr);
+}
+
+console.log(fib(8));
